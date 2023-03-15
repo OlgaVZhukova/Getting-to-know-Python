36e2fc58870d056cdef9182b0f641aeb3b005c1e
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-15 18:00:31 +0300

    Method for start is added

--- Homeworks/HW10/sweet_game/bot.py
+++ Homeworks/HW10/sweet_game/bot.py
@@ -11,3 +11,17 @@ user_turn = 0
 bot_turn = 0
 flag = ""
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    global flag
+    bot.send_message(message.chat.id, "Приветствую вас в игре!")
+    bot.send_message(message.chat.id, f"Всего в игре {sweets} конфет")
+    flag = random.choice(["user", "bot"])
+    if flag == "user":
+        bot.send_message(message.chat.id, "Первый ваш ход.")
+        controller(message)
+    else:
+        bot.send_message(message.chat.id, "Первый мой ход.")
+        controller(message)
+
+
