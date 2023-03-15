26aeacabef10104fb5e61babbfdfae4fdff2cd90
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-15 17:56:24 +0300

    Import of bot and Initialization

---
+++ Homeworks/HW10/sweet_game/bot.py
@@ -0,0 +1,13 @@
+import telebot
+from telebot import types
+import random
+
+
+bot = telebot.TeleBot("5903419564:AAEQEnawCtrDpprHbJ9M-IG-kpbSiT4X2l0")
+
+sweets = 221
+max_sweet = 28
+user_turn = 0
+bot_turn = 0
+flag = ""
+
